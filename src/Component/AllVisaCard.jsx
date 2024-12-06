3b1e0311f1e876e3ff5decce9084e50991e62d5b
tasnimalam1 <rafsantasnimrafsan@gmail.com>
2024-12-06 23:41:26 +0600

    all visas page create and show all visas using card design. and visa application data remove functionality done

--- src/Component/AllVisaCard.jsx
+++ src/Component/AllVisaCard.jsx
@@ -15,9 +15,8 @@ const AllVisaCard = ({ visa }) => {
     validity,
     applicationMethod,
   } = visa;
-  console.log(_id)
-  // const {_id,countryImage,countryName,visaType,processingTime,requiredDocuments,description,ageRestriction,fee,validity,applicationMethod} = visa;
-  // console.log(visaData)
+  
+  
   const navigate = useNavigate();
   return (
     <div>
